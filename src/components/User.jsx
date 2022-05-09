81662ea670715ed25c3f906df81316f2748644d0
emisasal <emiliano.sasal@gmail.com>
2022-05-09 11:22:45 -0300

    Componente SearchBar en commons

--- src/components/User.jsx
+++ src/components/User.jsx
@@ -13,30 +13,43 @@ const User = () => {
     dispatch(getFavShows())
   }, [dispatch])
 
-  if (!favMovies[0] || !favShows[0]) return <div></div>
+  if (!favMovies[0] && !favShows[0])
+    return (
+      <div className="container is-fullheight-not">
+        <br />
+        <h2 className="title is-5 title-customreg">
+          You don't have any favourites yet!
+        </h2>
+      </div>
+    )
 
   return (
     <>
-      <div className="container">
+      <div className="container footer-custom">
         <br />
         <h1 className="title is-5">Welcome {user.name}!</h1>
         <div></div>
         <p className="subtitle is-6">
-          You have {favMovies.length}{" "}
-          {favMovies.length === 1 ? "movie" : "movies"} and {favShows.length}{" "}
+          You have {favMovies.length === 0 ? "no" : favMovies.length}{" "}
+          {favMovies.length === 1 ? "movie" : "movies"} and{" "}
+          {favShows.length === 0 ? "no" : favShows.length}{" "}
           {favShows.length === 1 ? "tv show" : "tv shows"} in your favourites
           list.
         </p>
         <hr />
         <h2 className="title is-5 title-custom">Movies</h2>
-        <br />
-        <FavGrid dataList={favMovies} type={"movie"} user={user} />
-        <br />
+        {favMovies[0] ? (
+          <>
+            <br />
+            <FavGrid dataList={favMovies} type={"movie"} user={user} />
+            <br />
+          </>
+        ) : null}
         <hr />
         <h2 className="title is-5 title-custom">Tv Shows</h2>
         <br />
         <FavGrid dataList={favShows} type={"tv"} user={user} />
-      <br />
+        <br />
       </div>
     </>
   )
